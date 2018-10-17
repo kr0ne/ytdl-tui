a9bef019e489e87eb86c200ed1dfffaa8e2c24f9
kr0ne <ogonek16@gmail.com>
2018-10-17 20:00:10 +0200

    Refactor ytdl-tui.py and add requirements.txt

--- ytdl-tui.py
+++ ytdl-tui.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
+import json
+import npyscreen
+import sys
 from subprocess import run
 from subprocess import check_output
-import json
-import sys
-import npyscreen
+
 
 class Fmt(object):
     def __init__(self, fmtId, text):
         self.fmtId = fmtId
         self.text = text
+
 
 class FmtList(npyscreen.SelectOne):
     def __init__(self, *args, **keywords):
@@ -19,12 +20,14 @@ class FmtList(npyscreen.SelectOne):
     def display_value(self, vl):
         return '{}'.format(vl.text)
 
-class TestApp(npyscreen.NPSApp):
-    audioFmts = []
-    videoFmts = []
-    url       = 'https://www.youtube.com/watch?v=2MpUj-Aua48'
 
-    def getSizeString(self, kilobytes):
+class TestApp(npyscreen.NPSApp):
+    audio_fmts = []
+    video_fmts = []
+    url = 'https://www.youtube.com/watch?v=2MpUj-Aua48'
+
+    @staticmethod
+    def get_size_string(kilobytes):
         unit = 'KiB'
         if kilobytes >= 1024:
             kilobytes = kilobytes / 1024
@@ -34,69 +37,71 @@ class TestApp(npyscreen.NPSApp):
             kilobytes = kilobytes / 1024
             unit = 'GiB'
 
-        return '{}{}'.format( round(kilobytes, 2), unit )
+        return '{}{}'.format(round(kilobytes, 2), unit)
 
-    def appendFmtToList(self, lst, fmtId, description, bitrate, kilobytes):
-        sizeString = self.getSizeString(kilobytes)
+    def append_fmt_to_list(self, lst, fmtId, description, bitrate, kilobytes):
+        sizeString = self.get_size_string(kilobytes)
         s = '{}\t- {}\t{}kbps'.format(fmtId, description, bitrate).expandtabs(2)
         s = '{}\t{}'.format(s, sizeString).expandtabs(8)
-        lst.append( Fmt(fmtId, s) )
+        lst.append(Fmt(fmtId, s))
 
-    def downloadJson(self):
+    def download_json(self):
         if len(sys.argv) > 1:
             self.url = sys.argv[1]
 
-        print( 'Querying formats for {}'.format(self.url) )
-        result = check_output( [ 'youtube-dl -j {}'.format(self.url) ], shell=True ).decode('utf-8')
-        #result = run( [ 'youtube-dl -j {}'.format(self.url) ], shell=True, capture_output=True, text=True )
-        #j = json.loads(result.stdout)
+        print('Querying formats for {}'.format(self.url))
+        result = check_output(['youtube-dl -j {}'.format(self.url)], shell=True).decode('utf-8')
+        # result = run( [ 'youtube-dl -j {}'.format(self.url) ], shell=True, capture_output=True, text=True )
+        # j = json.loads(result.stdout)
         return json.loads(result)
 
-    def fillModels(self):
-        j = self.downloadJson()
+    def fill_models(self):
+        j = self.download_json()
         duration = j['duration']
         fmts = j['formats']
 
         for fmt in fmts:
             try:
-                fmtId = fmt['format_id']
+                fmt_id = fmt['format_id']
                 size = fmt['filesize']
                 kilobytes = size / 1024
-                bitrate = round( (kilobytes / duration) * 8 )
+                bitrate = round((kilobytes / duration) * 8)
 
                 if fmt['vcodec'] and fmt['vcodec'] != 'none':
                     height = fmt['height']
                     desc = '{}p'.format(height)
-                    self.appendFmtToList(self.videoFmts, fmtId, desc, bitrate, kilobytes)
+                    self.append_fmt_to_list(self.video_fmts, fmt_id, desc, bitrate, kilobytes)
                 else:
                     acodec = fmt['acodec'][:4]
-                    self.appendFmtToList(self.audioFmts, fmtId, acodec, bitrate, kilobytes)
-            except KeyError as e:
+                    self.append_fmt_to_list(self.audio_fmts, fmt_id, acodec, bitrate, kilobytes)
+            except KeyError:
                 print('Unknown id/codec.')
 
     def main(self):
-        self.fillModels()
+        self.fill_models()
 
-        F  = npyscreen.Form(name = "Select preferred formats", minimum_lines=20)
+        formats = npyscreen.Form(name="Select preferred formats", minimum_lines=20)
 
-        F.add(npyscreen.FixedText, value = "Video", max_width=40, editable=False)
-        vidList = F.add(FmtList, value = [0], name="Video", max_width=40, max_height=16,
-                values = self.videoFmts, scroll_exit=True, exit_right=True)
+        formats.add(npyscreen.FixedText, value="Video", max_width=40, editable=False)
+        video = formats.add(FmtList, value=[0], name="Video", max_width=40, max_height=16,
+                            values=self.video_fmts, scroll_exit=True, exit_right=True)
 
-        F.add(npyscreen.FixedText, value = "Audio", max_width=40, editable=False, relx=42, rely=2)
-        audList = F.add(FmtList, value = [0], name="Audio", max_width=40, max_height=16,
-                values = self.audioFmts, scroll_exit=True, exit_left=True, relx=42, rely=3)
+        formats.add(npyscreen.FixedText, value="Audio", max_width=40, editable=False, relx=42, rely=2)
+        audio = formats.add(FmtList, value=[0], name="Audio", max_width=40, max_height=16,
+                            values=self.audio_fmts, scroll_exit=True, exit_left=True, relx=42, rely=3)
 
-        #This lets the user interact with the Form.
-        F.edit()
+        # This lets the user interact with the Form.
+        formats.edit()
 
         import curses
         curses.endwin()
 
-        ids = [ self.videoFmts[ vidList.value[0] ].fmtId, self.audioFmts[ audList.value[0] ].fmtId ]
-        prefs = '{}+{}'.format( ids[0], ids[1]  )
+        ids = [self.video_fmts[video.value[0]].fmtId,
+               self.audio_fmts[audio.value[0]].fmtId]
+        prefs = '{}+{}'.format(ids[0], ids[1])
         print(prefs)
-        run( [ "mpv --term-status-msg='Video bitrate: ${{video-bitrate}}, audio bitrate: ${{audio-bitrate}}' --ytdl-format {} {}".format(prefs, self.url) ], shell=True )
+        run(["mpv --term-status-msg='Video bitrate: ${{video-bitrate}}, audio bitrate: ${{audio-bitrate}}' --ytdl-format {} {}".format(
+            prefs, self.url)], shell=True)
 
 
 if __name__ == "__main__":
